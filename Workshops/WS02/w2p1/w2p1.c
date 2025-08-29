03585a32c11c6ee61e267b42f95dd75fea41bc52
BernaKutukcu <brnkutukcu55@gmail.com>
2025-08-30 01:37:45 +0300

    update

--- Workshops/WS02/w2p1/w2p1.c
+++ Workshops/WS02/w2p1/w2p1.c
@@ -1,17 +1,21 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<stdio.h>
 int main(){
+    const double TAX = 0.13;
+    const char patSize = 'S';
+    
     double small, medium, large;
+    int priceSmallCents, priceMediumCents, priceLargeCents;
 
     int shirtCount;
     int sub_total= 0;
     int total;
     int taxes;
-
-    int priceSmallCents, priceMediumCents, priceLargeCents;
     
     printf("Set Shirt Prices\n");
     printf("================\n");
-
     printf("Enter the price for a SMALL shirt: $");
     scanf("%lf", &small);
     printf("Enter the price for a MEDIUM shirt: $");
@@ -19,9 +23,9 @@ int main(){
     printf("Enter the price for a LARGE shirt: $");
     scanf("%lf", &large);
     printf("\n");
+
     printf("Shirt Store Price List\n");
     printf("======================\n");
-
     printf("SMALL  : $%.2f\n", small);
     printf("MEDIUM : $%.2f\n", medium);
     printf("LARGE  : $%.2f\n\n", large);
@@ -31,7 +35,7 @@ int main(){
     priceLargeCents = (int)(large * 100 + 0.5);
 
 
-    printf("Patty's shirt size is 'S'\n");
+    printf("Patty's shirt size is '%c'\n", patSize);
     printf("Number of shirts Patty is buying: ");
     scanf("%d", &shirtCount);
     printf("\n");
@@ -40,13 +44,13 @@ int main(){
     printf("Contains : %d shirts\n", shirtCount);
 
     sub_total = priceSmallCents * shirtCount;
-    printf("Sub-total: $%d.%04d\n", sub_total / 100, (sub_total % 100) * 100);
+    printf("Sub-total: $%.4f\n", sub_total / 100.0);
 
-    taxes = (sub_total * 13 + 50) / 100;
-    printf("Taxes    : $ %d.%04d\n", taxes / 100, (taxes % 100) * 100);
+    taxes = sub_total * TAX + 0.5;
+    printf("Taxes    : $ %.4f\n", taxes / 100.0);
     
     total = sub_total + taxes;
-    printf("Total    : $%d.%04d\n", total / 100, (total % 100) * 100);
+    printf("Total    : $%.4f\n", total / 100.0);
     
      return 0;
 }
